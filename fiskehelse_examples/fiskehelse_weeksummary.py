961068214b7053f3bb7a3c75db4daba22f66d376
BOUVET\trond.hanssen <trond.hanssen@bouvet.no>
2019-10-03 15:38:27 +0200

    offboarding

---
+++ fiskehelse_examples/fiskehelse_weeksummary.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from pprint import pprint
+from credentials_thh import config
+
+token ={}
+
+def get_token():
+      req = requests.post("https://www.barentswatch.no/api/token",
+            data={
+                  'grant_type': 'client_credentials',
+                  'client_id': config['api_user'],
+                  'client_secret': config['api_password']
+            },
+            params={},
+            headers={'content-type': 'application/x-www-form-urlencoded'})
+ 
+      return req.json()
+
+def get_week_summary(token, year, week):
+	url = "https://www.barentswatch.no/api/v1/geodata/fishhealth/locality/"+year+"/"+week
+	token_bearer = token['access_token'].encode("ascii", "ignore")
+	headers ={
+		'authorization': "Bearer " + token_bearer,
+        'content-type': "application/json",
+	}
+
+	response = requests.get(url, headers=headers)
+	if response.status_code == requests.codes.ok:
+		return response.json()
+	else:
+		return "Error"
+
+
+token = get_token()
+print token
+
+weeksummary= get_week_summary(token,"2017","45")
+pprint(weeksummary)
